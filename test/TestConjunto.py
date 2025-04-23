bd6ceabd46cc250b7cef54151d655893165063bc
70290239 <70290239@continental.edu.pe>
2025-04-23 11:50:02 -0500

    Caso 4: Calcular promedio con un conjunto de n elementos (Rojo)

--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -1,4 +1,5 @@
 import unittest
+
 from src.logica.Conjunto import Conjunto
 
 
@@ -24,3 +25,10 @@ class TestConjunto(unittest.TestCase):
 
         conjunto = Conjunto([5, 7])
         self.assertEqual(6, conjunto.promedio())
+
+
+    def test_conjunto_nElementos_retornaPromedioNElementos(self):
+
+
+        conjunto = Conjunto([2, 4, 8, 9, 10, 15])
+        self.assertEqual((2 + 4 + 8 + 9 + 10 + 15) / 6, conjunto.promedio())
